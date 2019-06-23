59874cfa65676010e86d7bd63b7cc052d976c8c8
Joy Lin <vvlnote@hotmail.com>
2019-06-23 22:47:16 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,2 +1,17 @@
 // using this file is optional
 // you can also load the code snippets in using your browser's console
+const main = document.getElementById('main');
+
+main.addEventListener('click', function(event) {
+  alert('I was clicked!');
+})
+
+const input = document.querySelector('input');
+input.addEventListener('keydonw', function(e) {
+  if (e.which === 71) {
+    console.log('default prevented');
+    return e.preventDefault();
+  } else {
+    console.log(`${e}, not a "g"`);
+  }
+})
